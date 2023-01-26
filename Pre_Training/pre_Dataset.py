17102fa0b6a80da9f55c8bf264bc23051f7b4c45
Z_legolas <32036515+Zlegolas@users.noreply.github.com>
2023-01-26 18:01:32 +0800

    Update pre_Dataset.py

--- Pre_Training/pre_Dataset.py
+++ Pre_Training/pre_Dataset.py
@@ -5,10 +5,7 @@ import torch
 class pre_Dataset(data.Dataset):
     def __init__(self, data_tensor, target_tensor):
         super(pre_Dataset, self).__init__()
-        """
-        data_tensor:由n个样本组成的n*4维矩阵；
-        target_tensor:n*1维矩阵真实的值；
-        """
+
         self.data_tensor = torch.tensor(data_tensor)
         self.target_tensor = torch.tensor(target_tensor)
 
